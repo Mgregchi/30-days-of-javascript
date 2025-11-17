68bc495f96760c30c275f28976baf0c3ef8700cf
Michael. C. A <gm38929@gmail.com>
2025-11-17 09:44:08 +0100

    Push day 18

---
+++ day-19-parallel-asynchronous-functions.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function (functions) {};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
